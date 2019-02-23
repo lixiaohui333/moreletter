d029e5992cde50c93b9614453a420e97329d1eeb
矢催 <veniayang.yxl@antfin.com>
2019-02-24 01:18:01 +0800

    ³õʼ»¯ÏîĿ

---
+++ moreletter_server/src/main/java/com/free/moreletter/MoreletterApplication.java
@@ -0,0 +1,13 @@
+package com.free.moreletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreletterApplication.class, args);
+	}
+
+}
